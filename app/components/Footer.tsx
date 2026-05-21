e3d9b16700423630f78df1eddb719dd0febe1751
Vinicius Roveran <viniroveran@gmail.com>
2026-05-21 10:41:33 -0300

    feat: improve header and footer font color and size

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export function Footer() {
   return (
-    <footer className="bg-linear-to-t from-gray-700 to-gray-900 rounded-2xl shadow-md py-2 px-8 m-4 text-center text-md">
+    <footer className="bg-gray-800 rounded-3xl shadow-md py-2 px-8 m-4 text-center text-md font-bold text-gray-500">
       <div className="container mx-auto">
         <p>&copy; {new Date().getFullYear()} Dumbledore Dev. All rights reserved.</p>
       </div>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,9 +4,9 @@ import React from 'react';
 
 export function Header() {
   return (
-    <header className="bg-linear-to-b from-gray-700 to-gray-900 shadow-md rounded-2xl py-4 px-8 m-4">
+    <header className="bg-gray-800 shadow-md rounded-3xl m-4 p-1">
       <div className="container mx-auto flex justify-center items-center">
-        <h1 className="text-3xl font-bold text-gray-200">
+        <h1 className="text-3xl font-bold text-gray-500">
           SAST Vulnerability Dashboard
         </h1>
       </div>
